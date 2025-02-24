74de630641dd597a6915f9c9fe99c57894bf9c49
Daiwery <andrey.luk10@gmail.com>
2025-02-24 23:15:20 +0300

    Добавлена прямая задача для Z3, начат код для обратной задач

--- include/Z3.hpp
+++ include/Z3.hpp
@@ -27,57 +27,65 @@ double F_symmetrical(double h_i, double h_j, double l_ij, double l_ik, bool arg_
 /**
  * @brief Структура для решение прямой и обратной задач для 
  * трехчастичной статистической суммы в симметричном случае.
- * Ограничения для прямой задачи: l_ij > 0, l_ik > 0.
- * Ограничения для обратной задачи: mi = mj = m, eta_ij > m*m, eta_ik > m*m, eta_ik > eta_ij*eta_ij
+ * Ограничения: l_ij > 0, l_ik > 0.
  */
-// struct Z3_symmetrical {
-// private:
-//     double _h, _l, sqrt_l, _sqrt_l;
-//     double _exp_2h, _exp_2l;
-//     double cF2_norm, E12_norm, E1_norm, E_norm;
-//     double m_llb, m_h_llb, eta_prmg;
-//     double m_h;
+struct Z3_symmetrical {
+private:
+    double kappa, _kappa;
+    double sqrt_h_i, sqrt_h_j, sqrt_l_ij, sqrt_l_ik;
+    double _exp_2h_i, _exp_2h_j, _exp_2l_ij, _exp_2l_ik;
+    double cF3, _cF3;
+    double E123, E12, E1, E13, E3, E;
+    double FTilde3, FTilde123, FTilde12;
+    double F13, F, F1;
 
-//     void calc_base();
+    void calc_base();
 
-//     void calc_mi();
-//     void calc_mj();
-//     void calc_eta_ij();
-//     void calc_eta_ik();
-//     void calc_mj_eta_ij();
-//     void calc_mjh2();
-//     void calc_Qstar();
+    void calc_mi();
+    void calc_mj();
+    void calc_eta_ij();
+    void calc_eta_ik();
 
-// public:
-//     double h_i, h_j, l_ij, l_ik;
-//     double mi, mj, m, eta_ij, eta_ik;
-//     double mj_eta_ij, mjh2, Qstar;
-//     double Z2_norm;
+    void calc_mj_eta_ij();
+    void calc_mi_eta_ik();
+    void calc_mj_eta_ik();
+    void calc_eta_ik2();
 
-//     /**
-//      * @brief Рассчитывает прямую задачу: coeffs -> moments.
-//      * Значение коэффициентов задаются через поля.
-//      * Ограничения: l_ij > 0, l_ik > 0.
-//      * 
-//      */
-//     void calc_from_coeffs();
+    void calc_mjh2();
+    void calc_Q_star();
 
-//     /**
-//      * @brief Решает обратную задачу: m, eta_ij, eta_ij -> h, l -> moments.
-//      * Значение m (не mi и mj (!)), eta_ij, eta_ij задаются через поля.
-//      * Ограничения: mi = mj = m, eta_ij > m*m, eta_ik > m*m, eta_ik > eta_ij*eta_ij.
-//      * Значение коэффициентов хранятся как поля.
-//      * После нахождения коэффициентов рассчитывает все моменты (mi, mj, eta_ij, eta_ij пересчитываются).
-//      * 
-//      * @param hi Начальное значение hi.
-//      * @param hj Начальное значение hj.
-//      * @param lij Начальное значение lij.
-//      * @param lik Начальное значение lik.
-//      * @param eps Невязка.
-//      * @param max_steps Максимально допустимое количество шагов.
-//      * @return int Количество шагов метода Ньютона.
-//      */
-//     int calc_from_moments(double hi0, double hj0, double lij0, double lik0, double eps = 1e-8, int max_steps = 64);
-// };
+    void calc_Z3_norm();
+
+public:
+    double h_i, h_j, l_ij, l_ik;
+    double mi, mj, eta_ij, eta_ik;
+    double mj_eta_ij, mi_eta_ik, mj_eta_ik, eta_ik2;
+    double mjh2, Q_star;
+    double Z3_norm;
+
+    /**
+     * @brief Рассчитывает прямую задачу: coeffs -> moments.
+     * Значение коэффициентов задаются через поля.
+     * Ограничения: l_ij > 0, l_ik > 0.
+     */
+    void calc_from_coeffs();
+
+    /**
+     * @brief Решает обратную задачу: mi, mj, eta_ij, eta_ij -> h, l -> moments.
+     * Значение mi, mj, eta_ij, eta_ij задаются через поля.
+     * Ограничения:l_ij > 0, l_ik > 0.
+     * Значение коэффициентов хранятся как поля.
+     * После нахождения коэффициентов рассчитывает все моменты (mi, mj, eta_ij, eta_ij пересчитываются).
+     * 
+     * @param hi Начальное значение hi.
+     * @param hj Начальное значение hj.
+     * @param lij Начальное значение lij.
+     * @param lik Начальное значение lik.
+     * @param eps Невязка.
+     * @param max_steps Максимально допустимое количество шагов.
+     * @return int Количество шагов метода Ньютона.
+     */
+    // int calc_from_moments(double hi0, double hj0, double lij0, double lik0, double eps = 1e-8, int max_steps = 64);
+};
 
 #endif  // CMDLIB_Z3_HPP
